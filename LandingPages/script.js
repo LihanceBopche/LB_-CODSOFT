e5da73b452ed9d38c71775b324fd9d033e74dc1e
LB_CODSOFT <lihancebopche@gmail.com>
2024-07-23 12:25:09 +0530

    Add new folder Landing page

--- LandingPages/script.js
+++
@@ -1,7 +0,0 @@
-const toggel = document.getElementById("toggel");
-const navlist = document.getElementById("navlist");
-
-toggel.addEventListener("click", () => {
-    navlist.classList.toggle("navlist-active")
-});
-
